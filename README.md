61150a3a4efb1435b7b008c4388d13dfc26d0cf0
SubhojitGuin <subhojitguin2004@gmail.com>
2023-06-15 16:50:38 +0530

    Add table, header, total price, company name and logo.

---
+++ README.md
@@ -0,0 +1,52 @@
+# Invoice Conversion to PDF
+
+This Python script, `main.py`, converts Excel invoice files to PDF format using the `fpdf` library. It iterates through a list of Excel file paths in the `invoices` directory and generates a PDF file for each invoice.
+
+## Features
+
+- Converts Excel invoice files to PDF format.
+- Extracts the invoice number and date from the file name.
+- Adds the invoice number and date as a header in the PDF.
+- Reads the invoice data from the Excel file and adds it to a table in the PDF.
+- Calculates and displays the total price at the end of the table.
+- Adds the company name and logo to the PDF.
+- Saves the generated PDF files in the `PDFs` directory.
+
+## File Structure
+
+- `main.py`: This script contains the main code for converting Excel invoices to PDFs.
+- `invoices/`: This directory contains the input Excel invoice files.
+- `PDFs/`: This directory stores the generated PDF files.
+- `sg-logo-design-initial-sg-letter-logo-icon-design-pro-free-vector.jpg`: The company logo image file.
+
+## Usage
+
+1. Place the Excel invoice files in the `invoices` directory.
+2. Run the `main.py` script using Python: `python main.py`.
+3. The script will convert each Excel invoice file to a PDF and save it in the `PDFs` directory.
+4. The generated PDF files will have the same name as the input Excel files.
+
+## Requirements
+
+The project requires the following dependencies:
+
+- fpdf==1.7.2
+- pandas==1.3.3
+- openpyxl==3.0.9
+
+You can install the required dependencies by running `pip install -r requirements.txt`.
+
+## Customization
+
+You can customize the script by modifying the following:
+
+- Input Directory: Change the directory path in the `glob.glob()` function to specify the directory where the Excel invoice files are located.
+- Output Directory: Modify the `PDFs/` directory path in the `pdf.output()` function to specify the directory where the generated PDF files should be saved.
+- Logo: Replace the `sg-logo-design-initial-sg-letter-logo-icon-design-pro-free-vector.jpg` file with your company logo file.
+- Styling: Modify the font, color, and layout settings to customize the appearance of the PDF output.
+
+## Acknowledgements
+
+- [fpdf](https://github.com/reingart/pyfpdf): A Python library for PDF document generation.
+
+Feel free to customize the above template to include specific details about your invoice conversion project, such as additional features, instructions, or any other relevant information.
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
+# To extract a list of the filepaths of the files
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
+    # To extract the date and invoice nr.
     filename = Path(filepath).stem
     invoice_nr, date = filename.split("-")
 
@@ -18,6 +18,48 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1, align="L")
 
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1, align="L")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1, align="L")
+
+    # To add the header
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    columns = df.columns
+    columns = [column.replace("_", " ").title() for column in columns]
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, txt=columns[0], border=1)
+    pdf.cell(w=65, h=10, txt=columns[1], border=1)
+    pdf.cell(w=40, h=10, txt=columns[2], border=1)
+    pdf.cell(w=30, h=10, txt=columns[3], border=1)
+    pdf.cell(w=30, h=10, txt=columns[4], border=1, ln=1)
+
+    # To add the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=10, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=65, h=10, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=10, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=10, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=10, txt=str(row["total_price"]), border=1, ln=1)
+
+    # To add the total price
+    total = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=10, border=1)
+    pdf.cell(w=65, h=10, border=1)
+    pdf.cell(w=40, h=10, border=1)
+    pdf.cell(w=30, h=10, border=1)
+    pdf.cell(w=30, h=10, txt=str(total), border=1, ln=1)
+
+    # To add the total amount, company name and logo
+    pdf.ln(20)
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=0, h=10, txt=f"The total due amount is Rs. {total}.", ln=1)
+    pdf.cell(w=26, h=10, txt="SubhojitGuin")
+    pdf.image("sg-logo-design-initial-sg-letter-logo-icon-design-pro-free-vector.jpg",
+              w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
